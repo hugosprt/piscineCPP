34690166c29bb17917b8af3aa19efa9bbf95a7ca
vboxuser <vboxuser@ubuntu.myguest.virtualbox.org>
2023-02-10 14:59:07 +0100

    ex00

---
+++ CPP01/ex00/Zombie.cpp
@@ -0,0 +1,17 @@
+#include "Zombie.hpp"
+
+
+
+Zombie::Zombie(std::string name){
+    this->_name = name;
+    return ;
+}
+
+Zombie::~Zombie(void){
+   std::cout << "Zombie instance <" << this->_name << "> destroyed." << std::endl;
+   return ;
+}
+
+void    Zombie::announce(void){
+    std::cout << "<" << this->_name << "> : BraiiiiiiinnnzzzZ..." << std::endl;
+}
---
+++ CPP01/ex00/Zombie.hpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <iomanip>
+#include <iostream>
+
+class Zombie{
+    private:
+    std::string _name;
+
+    public:
+    Zombie(std::string name);
+    ~Zombie(void);
+    void    announce(void);
+};
+    Zombie* newZombie( std::string name);
+    void randomChump( std::string name);
